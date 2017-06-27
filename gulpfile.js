3facf4efc76c283f4c2781b5777e955855612312
Marilyn Carlin <marilyn.s.carlin@gmail.com>
2017-06-27 09:53:46 -0700

    complete project per specifications of lesson

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
-var gulp = require("gulp");
-var browserify = require("browserify");
-var source = require("vinyl-source-stream");
-var concat = require("gulp-concat");
+var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var utilities = require('gulp-util');
+var del = require('del');
+var jshint = require('gulp-jshint');
+var buildProduction = utilities.env.production;
 
 gulp.task('concatInterface', function() {
   return gulp.src(['./js/*-interface.js'])
@@ -9,9 +14,33 @@ gulp.task('concatInterface', function() {
   .pipe(gulp.dest('./tmp'));
 });
 
-gulp.task("jsBrowserify", ['concatInterface'], function() {
+gulp.task('jsBrowserify', ['concatInterface'], function() {
   return browserify({ entries: ["./tmp/allConcat.js"] })
   .bundle()
   .pipe(source("app.js"))
   .pipe(gulp.dest("./build/js"));
+});
+
+gulp.task('minifyScripts', ['jsBrowserify'], function(){
+  return gulp.src("./build/js/app.js")
+  pipe(uglify())
+  .pipe(gulp.dest("./build/js"));
+});
+
+gulp.task('clean', function(){
+  return del(['build', 'tmp']);
+});
+
+gulp.task('build', ['clean'], function(){
+  if (buildProduction) {
+    gulp.start('minifyScripts');
+  } else {
+    gulp.start('jsBrowserify');
+  }
+});
+
+gulp.task('jshint', function(){
+  return gulp.src(['js/*.js'])
+  .pipe(jshint())
+  .pipe(jshint.reporter('default'));
 });
